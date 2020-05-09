714508aa176956c9e635307bb73cf8bb4131eefe
Adam Kauffman <akauffman33@gmail.com>
2020-05-09 14:22:21 -0700

    Full Code Review
    Remove binaries from source control
    Rename generic defaults
    Update to .NET 4.7.2
    Dynamic Build #
    Allow only one instance to run at a time
    Declare shared strings once
    Implement Dispose pattern
    Exit Thread gracefully
    Detect Monitor Refresh rate
    Never Thread.Abort, use cancel flag
    Clean up event handlers during shutdown prevent
    Comment out unused code
    Misc Refactoring

---
+++ CenterTaskbar/DisplaySettings.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CenterTaskbar
+{
+    static class DisplaySettings
+    {
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+        const int ENUM_CURRENT_SETTINGS = -1;
+        //const int ENUM_REGISTRY_SETTINGS = -2;
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct DEVMODE
+        {
+
+            private const int CCHDEVICENAME = 0x20;
+            private const int CCHFORMNAME = 0x20;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public ScreenOrientation dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+
+        }
+
+        public static void ListAllDisplayModes()
+        {
+            DEVMODE vDevMode = new DEVMODE();
+            int i = 0;
+            while (EnumDisplaySettings(null, i, ref vDevMode))
+            {
+                Console.WriteLine("Width:{0} Height:{1} Color:{2} Frequency:{3}",
+                                        vDevMode.dmPelsWidth,
+                                        vDevMode.dmPelsHeight,
+                                        1 << vDevMode.dmBitsPerPel, vDevMode.dmDisplayFrequency
+                                    );
+                i++;
+            }
+        }
+
+        public static int CurrentRefreshRate()
+        {
+            DEVMODE vDevMode = new DEVMODE();
+            if (EnumDisplaySettings(null, ENUM_CURRENT_SETTINGS, ref vDevMode)) {
+                return vDevMode.dmDisplayFrequency;
+            } else
+            {
+                return 60;
+            }
+        }
+    }
+}
--- CenterTaskbar/Properties/Resources.Designer.cs
+++ CenterTaskbar/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace CenterTaskbar.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -63,9 +63,9 @@ namespace CenterTaskbar.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Icon Icon1 {
+        internal static System.Drawing.Icon TrayIcon {
             get {
-                object obj = ResourceManager.GetObject("Icon1", resourceCulture);
+                object obj = ResourceManager.GetObject("TrayIcon", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
